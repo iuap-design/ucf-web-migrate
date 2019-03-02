d3f65adf06f4bf30217727ac0596aa0a98b22fd1
wangxgk <wangxgk@yonyou.com>
2019-03-02 22:47:37 +0800

    发布测试版

--- src/index.js
+++ src/index.js
@@ -12,16 +12,23 @@ let demoDir = 'D:/Develop/web/iuap_pap_react'
 let demo = path.join(__dirname, '../test/demo_modules');
 // let demo = path.join(__dirname, '../test/demo_pages');
 module.exports = (options) => {
-    // console.log(options)
+    // console.log(__dirname)
+    // console.log(process.cwd())
+    let filePath = process.cwd();
+    // console.log(options, filePath);
+    // return;
     //创建备份目录
-    createCodBack(demo).then(()=>{
+    createCodBack(filePath).then(()=>{
+        console.log('备份文件创建完成')
         //生成ucf.config.js 和 package.json
-        return createUcfCfg(demo)
+        return createUcfCfg(filePath)
     }).then(()=>{
+        console.log('配置文件创建完成')
+        console.log('正在生成新的项目结构')
         //整理并生成ucf项目结构。
-        createNewDir(demo)
+        createNewDir(filePath);
     }).then(() =>{
-        console.log('all done')
+        console.log('目录创建完成')
     }).catch(err=>{
         console.log(err.stack);
     })
